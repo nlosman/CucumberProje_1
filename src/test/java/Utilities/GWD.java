46cbb004bc12cce524fde0df6ed51cc481926eff
HP <osmanenosman@hotmail.com>
2023-09-22 16:56:51 +0200

    Initial commit

---
+++ src/test/java/Utilities/GWD.java
@@ -0,0 +1,43 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.Locale;
+
+public class GWD {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        // extend report türkçe bilg çalışmaması sebebiyle eklendi
+        Locale.setDefault(new Locale("EN"));
+        System.setProperty("user.language", "EN");
+
+        if (driver==null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
+        }
+
+        return driver;
+    }
+
+
+    public static void quitDriver() {
+
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        if (driver!=null) { //driver var ise
+           driver.quit();
+           driver=null;
+        }
+    }
+
+
+}
