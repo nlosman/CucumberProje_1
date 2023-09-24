a51dc4a464b227eec8b823b3803bc75140db0686
selimOzal <selimozalnl@gmail.com>
2023-09-24 11:28:25 +0200

    US_8 is under development.

--- src/test/java/FeatureFiles/US_8_Selim.feature
+++ src/test/java/FeatureFiles/US_8_Selim.feature
@@ -1,1 +1,9 @@
-Feature:
+Feature: Login functionality
+
+ Scenario: Login with valod Contact Us
+
+Given Navigate to Cleverppc
+
+  When Enter ContactUs and click button
+  Then Send a message
+  And  User can successfully receive the message
--- src/test/java/Pages/Selim.java
+++ src/test/java/Pages/Selim.java
@@ -1,4 +1,40 @@
 package Pages;
 
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Selim extends Parent{
+    public Selim() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[@id='contact-link']/a")
+    public WebElement ContactUsLoginButton;
+
+
+    @FindBy(id="uniform-id_contact")
+    public WebElement SubjectHeading;
+
+    @FindBy(id="id_contact")
+    public WebElement Customer;
+
+    @FindBy(id="email")
+    public WebElement emailAdress;
+
+    @FindBy(id="id_order")
+    public WebElement OrderReference;
+
+    @FindBy(id="uniform-fileUpload")
+    public WebElement chooseFile;
+
+    @FindBy(id="message")
+    public WebElement message;
+
+    @FindBy(xpath = "//*[@id='submitMessage']/span")
+    public WebElement sendButton;
+
+    @FindBy(xpath = "//*[@id='center_column']/p")
+    public WebElement actualText;
 }
