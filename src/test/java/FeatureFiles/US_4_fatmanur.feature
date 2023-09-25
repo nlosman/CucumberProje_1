92ecda7cccccb5606d3718c6478bd32f5cd8eb60
111333444 <coban.fatmanur22@gmail.com>
2023-09-25 15:57:47 +0200

    Fatmanur test 2 yapildi

--- src/test/java/FeatureFiles/US_4_fatmanur.feature
+++ src/test/java/FeatureFiles/US_4_fatmanur.feature
@@ -1,2 +1,15 @@
 Feature: Add To Cart Functionality
 
+Scenario: Add To Cart
+
+  Given Navigate to Cleverppc
+  When Click Dresses button
+  Then Click Summer Dresses button
+  And Click on the element in the dress category
+  When Click add to cart button
+  And Success message should be displayed
+
+
+
+
+
---
+++ src/test/java/StepDefinitions/US_4_fatmanur.java
@@ -0,0 +1,41 @@
+package StepDefinitions;
+
+import Pages.Fatmanur;
+import Pages.Parent;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
+public class US_4_fatmanur {
+    Parent prn = new Parent();
+    Fatmanur ftnr = new Fatmanur();
+
+    @When("Click Dresses button")
+    public void clickDressesButton() {
+        ftnr.myClick(ftnr.dressesCategorie);
+    }
+
+    @Then("Click Summer Dresses button")
+    public void clickSummerDressesButton() {
+        ftnr.myClick(ftnr.sommerDrsBtn);
+    }
+
+    @And("Click on the element in the dress category")
+    public void clickOnTheElementInTheDressCategory() {
+        ftnr.myClick(ftnr.dressClick);
+    }
+
+    @When("Click add to cart button")
+    public void clickAddToCartButton() {
+        prn.wait.until(ExpectedConditions.urlToBe("https://cleverppc.com/prestashop4/index.php?id_product=6&controller=product&id_lang=1"));
+        ftnr.myClick(ftnr.addBtn);
+    }
+
+    @And("Success message should be displayed")
+    public void successMessageShouldBeDisplayed() {
+        prn.verifyContainsText(ftnr.successMsg, "successfully");
+
+    }
+}
