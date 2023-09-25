d8f3f40a7aec0788eb82713fbebd1d60a0c87b10
111333444 <coban.fatmanur22@gmail.com>
2023-09-25 17:15:42 +0200

    Fatmanur test calismasi

--- src/test/java/Pages/Fatmanur.java
+++ src/test/java/Pages/Fatmanur.java
@@ -1,4 +1,26 @@
 package Pages;
 
-public class Fatmanur extends Parent{
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Fatmanur extends Parent{//pages classi
+  public Fatmanur() {
+    PageFactory.initElements(GWD.getDriver(), this);}
+
+  @FindBy(xpath = "(//a[@class='sf-with-ul'])[4]" )
+  public WebElement dressesCategorie;
+
+  @FindBy (xpath = "(//li[@class='last'])[1]")
+  public WebElement sommerDrsBtn;
+
+  @FindBy (xpath = "(//a[@class='product-name'])[2]")
+  public WebElement dressClick;
+
+  @FindBy (css = "button[type='submit'][name='Submit']")
+  public WebElement addBtn;
+
+  @FindBy (xpath = "(//span[@class='title'])[1]")
+  public WebElement successMsg;
 }
